d28c6692aebe11ce3c425fc435f8dd8477053689
Daniel Santiago Aguila Torres <daniel.santiago.aguila.torres@gmail.com>
2023-11-10 12:21:27 -0800

    feat: add gemm code

---
+++ src/gemm_basic.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+float* gemm_basic(float* a, int n, int m, float* b, int p) {
+  if (n < 1 || m < 1 || p < 1)
+    return NULL;
+
+  float* c = malloc(sizeof(float) * n * p);
+  if (c == NULL) {
+    return NULL;
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < p; j++) {
+      c[i * p + j] = 0.0f;
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < p; j++) {
+      for (int k = 0; k < m; k++) {
+	c[i * p + j] += a[i * m + k] * b[k * p + j];
+      }
+    }
+  }
+
+  return c;
+}
---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "gemm_basic.c"
+
+
+int main() {
+  int n = 2;
+  int m = 3;
+  float a[2][3] = {
+    {1.0, 2.0, 3.0},
+    {5.0, 6.0, 7.0},
+  };
+
+  int p = 4;
+  float b[3][4] = {
+    {1.0, 2.0, 3.0, 4.0},
+    {5.0, 6.0, 7.0, 8.0},
+    {9.0, 10.0, 11.0, 12.0}
+  };
+
+  float* c = gemm_basic(&a[0][0], n, m, &b[0][0], p);
+
+  if (c) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < p; j++) {
+	printf("%f, ", c[i * p + j]);
+      }
+      printf("\n ");
+    }
+    free(c);
+  }
+
+  return 0;
+}
